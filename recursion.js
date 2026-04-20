83b82b844e6c34c15312e0a1b6428d1b0d79107e
Supritha Patil <142718780+Supritha-08@users.noreply.github.com>
2026-04-20 17:29:49 +0530

    Update recursion.js

--- recursion.js
+++ recursion.js
@@ -1,1 +1,13 @@
-
+#infinite loops
+function example(){
+  console.log('one');
+  example();
+}
+#Recursion function-alternative way to run the repeated function
+  let counter=3;
+  function  example(){
+    console.log(counter);
+    counter=counter-1;
+    if(counter===0)return;
+    example();
+  }
